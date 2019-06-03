ab7fc821f7ad85ed4bc4664f200f6dffe48a7518
Vadim Gordeev <enzogord@gmail.com>
2019-06-03 16:31:12 +0300

    Fix структура фильтра

--- QS.Project/Project.Journal/IJournalFilter.cs
+++ QS.Project/Project.Journal/IJournalFilter.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using NHibernate.Criterion;
 namespace QS.Project.Journal
 {
 	public interface IJournalFilter
 	{
+		ICriterion GetFilter();
+		event EventHandler OnFiltered;
+		void Update();
 	}
 }
